e8b5b107a498f998f549ef3b30f93d67f4d3dbca
Pepe García <pepe@pepegar.com>
2020-11-18 13:59:13 +0100

    initial commit

---
+++ session14.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+#%% Homework
+
+# 1. Create a graph in Python that represents a group of people andthe debts between them.
+# 2. Create a graph in Python that represents a subway map.
+# 3. Create a graph in Python that represents friendship in Facebook.
+# 4. Think of a way of implementing them using either adjacency lists or adjacency matrices
+
+#%% breadth first search
+
+#%% finding all paths from a given node
+
+#%% finding all paths from to a given node
